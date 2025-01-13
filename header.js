29bc11b094c8992a5ea957403f96830fb3087015
deco0926 <152508488+deco0926@users.noreply.github.com>
2025-01-13 21:32:43 +0800

    Update header.js

--- header.js
+++ header.js
@@ -4,10 +4,10 @@ document.addEventListener("DOMContentLoaded", function () {
             <div class="nav-wrapper indigo darken-3">
                 <a href="#" class="brand-logo">寶可夢介紹</a>
                 <ul id="nav-mobile" class="right hide-on-med-and-down">
-                    <li><a href="main.html">首頁</a></li>
-                    <li><a href="main.html">寶可夢介紹</a></li>
-                    <li><a href="main.html">人氣投票</a></li>
-                    <li><a href="main.html">寶可夢中心</a></li>
+                    <li><a href="index.html">首頁</a></li>
+                    <li><a href="introduce.html">寶可夢介紹</a></li>
+                    <li><a href="vote.html">人氣投票</a></li>
+                    <li><a href="center.html">寶可夢中心</a></li>
                 </ul>
             </div>
         </nav>
